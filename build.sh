f0945aee30f269e53c70140d52469c4a29d38457
Govardhan <gvdnreddy@gmail.com>
2019-03-05 11:41:46 +0530

    Commented build file to test

--- build.sh
+++ build.sh
@@ -1,27 +1,29 @@
 #!/bin/bash -e
-platform="Linux x86_64"
-if [ "$(uname -sm)" != "${platform}" ]; then
-    echo Must run ${platform}
-    exit 1
-fi
+# platform="Linux x86_64"
+# if [ "$(uname -sm)" != "${platform}" ]; then
+    # echo Must run ${platform}
+    # exit 1
+# fi
 
-# locale--independent sort order
-export LANG=
-# Use Go 1.11 modules and ignore GOPATH
-export GO111MODULE=on
+# # locale--independent sort order
+# export LANG=
+# # Use Go 1.11 modules and ignore GOPATH
+# export GO111MODULE=on
 
-# Creating version number with buildkite commit(7)
-echo --- Getting version
-height=$(git rev-list HEAD --first-parent --count)
-version=$(git rev-parse --short=7 ${BUILDKITE_COMMIT:-HEAD})
-full_version=${height}-${version}
-echo "Version:      ${version}"
-echo "Full version: ${full_version}"
+# # Creating version number with buildkite commit(7)
+# echo --- Getting version
+# height=$(git rev-list HEAD --first-parent --count)
+# version=$(git rev-parse --short=7 ${BUILDKITE_COMMIT:-HEAD})
+# full_version=${height}-${version}
+# echo "Version:      ${version}"
+# echo "Full version: ${full_version}"
 
-# integration testing for all the services including cloudwatch, storage, Ayla and core 
-pkg=github.com/pagidi/GoLang
+# # integration testing for all the services including cloudwatch, storage, Ayla and core 
+# pkg=github.com/pagidi/GoLang
+
+# echo --- start buiding
+# go build -o GoPractice-$(version)
+# echo --- End buiding
 
 echo --- start buiding
-go build -o GoPractice-$(version)
 echo --- End buiding
-
